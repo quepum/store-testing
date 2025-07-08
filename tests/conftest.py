5b4782c366f8791f1957f816ce1207baabada66c
quepum <118292850+quepum@users.noreply.github.com>
2025-07-09 00:05:43 +0300

    chore(conftest): Add --language parameter and set Chrome locale

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,28 +1,28 @@
-﻿import pytest
+﻿import os
+import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption("--language", action="store", default="en")
+
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
 
-    options = None
-    if browser_name == "chrome":
-        options = ChromeOptions()
-        options.add_argument(f"--lang={user_language}")
+    options = Options()
+    options.add_argument(f"--lang={user_language}")
+
+    # Headless только если мы в CI (GitHub Actions)
+    if os.getenv("GITHUB_ACTIONS") == "true":
         options.add_argument("--headless=new")
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        driver = webdriver.Chrome(options=options)
-    elif browser_name == "firefox":
-        options = FirefoxOptions()
-        options.set_preference("intl.accept_languages", user_language)
-        driver = webdriver.Firefox(options=options)
-    else:
-        raise pytest.UsageError("--browser_name is not correct. Use 'chrome' or 'firefox'.")
+        options.add_argument("--window-size=1920,1080")
 
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
