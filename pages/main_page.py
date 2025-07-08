9e679a1052a131c9bb664b8638e5eb8ce3080a23
quepum <118292850+quepum@users.noreply.github.com>
2025-07-09 01:55:28 +0300

    feat: add test for login link

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,9 @@
 ﻿from .base_page import BasePage
-from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
+from .login_page import LoginPage
+
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
-
-    def should_be_login_link(self):
-        assert self.browser.find_element(By.CSS_SELECTOR, "#login_link"), "Login link is not present."
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,15 +1,19 @@
-﻿from pages.main_page import MainPage
+﻿from pages.locators import MainPageLocators
+from pages.main_page import MainPage
+from pages.login_page import LoginPage
+
+link = "https://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "https://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
-
-def test_guest_should_see_login_link(browser):
-    link = "https://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
+
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, link)
+    page.open()
+    assert page.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not present."
