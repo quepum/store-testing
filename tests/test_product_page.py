fc96fb08707a63ee3aa8d29fce440400b1bc7662
quepum <118292850+quepum@users.noreply.github.com>
2025-07-09 20:37:57 +0300

    test: make the test parameterized

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,9 +1,13 @@
 ﻿from pages.product_page import ProductPage
+import pytest
 
-link = "https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+product_base_link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 
 
-def test_guest_can_add_product_to_basket(browser):
+@pytest.mark.parametrize('promo_offer',
+                         [pytest.param(i, marks=pytest.mark.xfail(i == 7, reason='')) for i in range(10)])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f"https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     page = ProductPage(browser, link)
     page.open()
 
@@ -11,5 +15,5 @@ def test_guest_can_add_product_to_basket(browser):
     page.add_to_basket()
     page.should_be_added_to_basket()
     product_in_message = page.get_success_message()
-    assert product_name in product_in_message, \
-        f"Product name in message '{product_in_message}' does not match expected '{product_name}'"
+    assert product_name == product_in_message, \
+        f"Product name in message does not match expected"
