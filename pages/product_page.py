2e5c8ed062dc0f20d61f30ea95310b1ee8ade997
quepum <118292850+quepum@users.noreply.github.com>
2025-07-09 19:43:05 +0300

    feat: create ProductPage class

---
+++ pages/product_page.py
@@ -0,0 +1,44 @@
+﻿from pages.base_page import BasePage
+from pages.locators import ProductPageLocators
+from selenium.common.exceptions import NoAlertPresentException
+import math
+
+
+class ProductPage(BasePage):
+
+    def add_to_basket(self):
+        self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON).click()
+        self.solve_quiz_and_get_code()
+
+    def get_product_name(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+
+    def get_product_price(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+
+    def get_success_message(self):
+        return self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE).text
+
+    def get_basket_total(self):
+        return self.browser.find_element(*ProductPageLocators.TOTAL_PRICE).text
+
+    def should_be_added_to_basket(self):
+        assert self.get_product_name() in self.get_success_message(), \
+            f"Название товара в корзине не совпадает ({self.get_product_name()}, {self.get_success_message()})"
+
+        assert self.get_product_price() in self.get_basket_total(), \
+            f"Цена товара {self.get_product_price()} и сумма корзины {self.get_basket_total()} не совпадают"
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
